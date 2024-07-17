7a1197056f11993c9ab0f0a96de62a0400af2076
Abeed <156498387+abeed04@users.noreply.github.com>
2024-07-17 10:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,16 +74,16 @@ def main():
     st.set_page_config("Chat with PDF")
     st.header("Chat with PDF using Llama3💁")
 
-    user_question = st.text_input("Ask a Question from the PDF Files uploaded")
+    user_question = st.text_input("Ask a Question from the PDF Files uploaded",
+                                 placeholder="Can you give me a short summary?",)
 
     if user_question:
         user_input(user_question)
 
 
     with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button",
-                                    accept_multiple_files=True)
+        st.title("Upload your PDF Files and Click on the Submit & Process Button")
+        pdf_docs = st.file_uploader("PDF Uploader", accept_multiple_files=True)
         if st.button("Submit & Process"):
             with st.spinner("Processing Pdf"):
                 raw_text = get_pdf_text(pdf_docs)
